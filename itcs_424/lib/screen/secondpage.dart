989402942ed80873935da13d5cda375f82bea760
PongsakornT050 <pongsakorn.tay@student.mahidol.edu>
2022-02-13 22:32:03 +0700

    Added a placeholder of hospitals by Pon, currently educating on list class flutter

--- itcs_424/lib/screen/secondpage.dart
+++ itcs_424/lib/screen/secondpage.dart
@@ -1,7 +1,7 @@
-//TODO: add services (Pon)
-
+//Added a placeholder of hospital list
 import 'dart:html';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class SecondRoute extends StatelessWidget {
   const SecondRoute({Key? key}) : super(key: key);
@@ -13,12 +13,87 @@ class SecondRoute extends StatelessWidget {
         title: const Text('Find Pet service'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator . pop( context );
-            
-          },
-          child: const Text('Go back!'),
+        child: Column(
+          children: [
+            Text('โรงพยาบาลสัตว์อาขิ',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์เคบีเพ็ท',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('เอื้ออาทร สัตวแพทย์',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์ แกรนด์อเวนิว',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์ พระราม 2',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์ซอฟท์เซนส์',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+                        ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์กัลปพฤกษ์',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์ใกล้หมอ',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์เอพลัส',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์ สาย1',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์บางกอก',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+            Text('โรงพยาบาลสัตว์สมใจ',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Book for appointment'),
+            ),
+
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            },
+            child: const Text('Go Back!'),
+            ),
+          ],
         ),
       ),
     );
